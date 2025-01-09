3d25098600a35466f2a535544e41aa4cec95db05
mstepanov214 <mstepanov214@gmail.com>
2025-01-10 00:28:07 +0300

    Add cookies support
    See https://github.com/yt-dlp/yt-dlp/wiki/FAQ#how-do-i-pass-cookies-to-yt-dlp and https://github.com/yt-dlp/yt-dlp/wiki/Extractors#exporting-youtube-cookies

--- MusicBroadcast/Youtube/YoutubeDataClient.cs
+++ MusicBroadcast/Youtube/YoutubeDataClient.cs
@@ -9,12 +9,19 @@ namespace MusicBroadcast.Youtube;
 internal static class YoutubeDataClient
 {
     static readonly YoutubeDL _ytdl = new();
+    private const string _cookiesPath = "cookies.txt";
 
     public static async Task<YoutubeData> GetYoutubeData(string url, CancellationToken ct = default)
     {
+        if (!File.Exists(_cookiesPath))
+        {
+            throw new FileNotFoundException("YouTube cookies file was not found.", _cookiesPath);
+        }
+
         var options = new OptionSet()
         {
-            ExtractorArgs = "youtube:skip=dash"
+            ExtractorArgs = "youtube:skip=dash",
+            Cookies = _cookiesPath
         };
 
         var videoDataFetch = await _ytdl.RunVideoDataFetch(url, ct, overrideOptions: options);
